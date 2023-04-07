3bedc44d54726235af500608e8577e8ca55c3f41
buflallo <hamza.lachkar@usmba.ac.ma>
2023-04-07 22:04:22 +0000

    lala

---
+++ srcs/requirements/mariadb/tools/script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+service mysql start;
+mysql -uroot -e "CREATE DATABASE IF NOT EXISTS \`TEST\`;"
+mysql -uroot -e "CREATE USER IF NOT EXISTS \`test\`@'%' IDENTIFIED BY 'Hanajay';"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON \`TEST\`.* TO \`test\`@'%' IDENTIFIED BY 'Hanajay';"
+mysql -uroot -e "FLUSH PRIVILEGES;"
+mysql -e "SET PASSWORD FOR root@localhost = PASSWORD('Hanajay')"
+sleep 3
+mysqladmin -u root -pHanajay shutdown
+exec mysqld_safe
---
+++ srcs/requirements/wordpress/tools/auto_config.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#service mysql start;
+
+#wp core config --allow-root --path='/var/www/wordpress/' --dbname='TEST' --dbuser='test' --dbpass='Hanajay' --dbhost='localhost' --dbprefix='wp_';
+
+#wp core install --allow-root --path='/var/www/wordpress/' --url='localhost' --title='Blog Title' --admin_user='adminuser' --admin_password='password' --admin_email='email@localhost';
+
+#wp user create --allow-root --path='/var/www/wordpress/' hlachkar bob@example.com --role=author --user_pass=Hanajay;

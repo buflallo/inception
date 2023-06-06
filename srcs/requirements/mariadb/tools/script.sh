86a616ea1524be14cd670fca121811e3a77f8fa0
buflallo <hamza.lachkar@usmba.ac.ma>
2023-06-06 16:00:44 +0100

    wordpress mariadb setup, binded volumes, docker compose ,network ...

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -4,7 +4,7 @@ mysql -uroot -e "CREATE DATABASE IF NOT EXISTS \`TEST\`;"
 mysql -uroot -e "CREATE USER IF NOT EXISTS \`test\`@'%' IDENTIFIED BY 'Hanajay';"
 mysql -uroot -e "GRANT ALL PRIVILEGES ON \`TEST\`.* TO \`test\`@'%' IDENTIFIED BY 'Hanajay';"
 mysql -uroot -e "FLUSH PRIVILEGES;"
-mysql -e "SET PASSWORD FOR root@localhost = PASSWORD('Hanajay')"
+mysql -e "SET PASSWORD FOR 'root'@'%' = PASSWORD('Hanajay')"
 sleep 3
 mysqladmin -u root -pHanajay shutdown
 exec mysqld_safe
--- srcs/requirements/wordpress/tools/auto_config.sh
+++ srcs/requirements/wordpress/tools/auto_config.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-#service mysql start;
+alias wp='/usr/local/bin/wp-cli.phar'
 
-#wp core config --allow-root --path='/var/www/wordpress/' --dbname='TEST' --dbuser='test' --dbpass='Hanajay' --dbhost='localhost' --dbprefix='wp_';
+wp core config --allow-root --path='/var/www/html/' --dbname='TEST' --dbuser='test' --dbpass='Hanajay' --dbhost='mariadb' --dbprefix='wp_';
 
-#wp core install --allow-root --path='/var/www/wordpress/' --url='localhost' --title='Blog Title' --admin_user='adminuser' --admin_password='password' --admin_email='email@localhost';
+wp core install --allow-root --path='/var/www/html/' --url='localhost' --title='Blog Title' --admin_user='adminuser' --admin_password='password' --admin_email='email@example.com';
 
-#wp user create --allow-root --path='/var/www/wordpress/' hlachkar bob@example.com --role=author --user_pass=Hanajay;
+wp user create --allow-root --path='/var/www/html/' hlachkar bob@example.com --role=author --user_pass=Hanajay;
+
+sleep 5;
+exec "$@"
